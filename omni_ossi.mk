5196c3861c4d8d121cc3cc9f33fb90292fc33b27
Adnan2109 <104997410+Adnan2109@users.noreply.github.com>
2025-12-22 11:42:15 +0100

    Update device identifiers and build properties

--- omni_ossi.mk
+++ omni_ossi.mk
@@ -1,29 +1,32 @@
 #
 # Copyright (C) 2025 The Android Open Source Project
-# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit from those products. Most specific first.
+# Inherit from core Android
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
+# Inherit Omni common config
 $(call inherit-product, vendor/omni/config/common.mk)
 
-# Inherit from ossi device
+# Inherit device configuration
 $(call inherit-product, device/oplus/ossi/device.mk)
 
+# Device identifiers
 PRODUCT_DEVICE := ossi
 PRODUCT_NAME := omni_ossi
-PRODUCT_BRAND := oplus
-PRODUCT_MODEL := ossi
+PRODUCT_SYSTEM_NAME := ossi
+PRODUCT_BRAND := OnePlus
+PRODUCT_MODEL := CPH2719
 PRODUCT_MANUFACTURER := oplus
 
+# GMS
 PRODUCT_GMS_CLIENTID_BASE := android-oplus
 
+# Build properties (match stock)
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="hal_mgvi_64_64only_armv82-user 14 UKQ1.231108.001 1763482850396 release-keys"
+    PRIVATE_BUILD_DESC="CPH2719EEA-user 14 UKQ1.231108.001 1763482850396 release-keys"
 
-BUILD_FINGERPRINT := oplus/ossi/ossi:14/UKQ1.231108.001/1763482850396:user/release-keys
+BUILD_FINGERPRINT := OnePlus/CPH2719EEA/ossi:14/UKQ1.231108.001/1763482850396:user/release-keys
