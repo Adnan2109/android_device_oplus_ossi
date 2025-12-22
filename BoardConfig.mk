a7b896770e70cce6d76869ba95e53c88007659d9
Adnan2109 <104997410+Adnan2109@users.noreply.github.com>
2025-12-22 12:21:04 +0100

    Modify BoardConfig.mk for OnePlus Nord CE5 5G
    Updated BoardConfig.mk for OnePlus Nord CE5 5G with new partitions and configurations.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,13 +1,15 @@
+#
+# BoardConfig.mk for OnePlus Nord CE5 5G (ossi)
+#
+
 DEVICE_PATH := device/oplus/ossi
+
 ALLOW_MISSING_DEPENDENCIES := true
 
-# A/B
+# A/B device
 AB_OTA_UPDATER := true
 BOARD_USES_RECOVERY_AS_BOOT := true
 BOARD_USES_INIT_BOOT := true
-BOARD_USES_VENDOR_BOOT := true
-TW_HAS_NO_RECOVERY_PARTITION := true
-TW_NO_REBOOT_BOOTLOADER := true
 
 AB_OTA_PARTITIONS += \
     system \
@@ -16,7 +18,9 @@ AB_OTA_PARTITIONS += \
     odm \
     vbmeta \
     vbmeta_system \
-    vbmeta_vendor
+    vbmeta_vendor \
+    vendor_boot \
+    init_boot
 
 # Architecture
 TARGET_ARCH := arm64
@@ -25,23 +29,26 @@ TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_VARIANT := generic
 TARGET_CPU_VARIANT_RUNTIME := cortex-a55
 
-TARGET_NO_BOOTLOADER := true
-TARGET_SCREEN_DENSITY := 480
-
-# Platform
+# Platform (MediaTek Dimensity 9000-series)
 TARGET_BOARD_PLATFORM := mt6897
 
-# Kernel
+# Bootloader
+TARGET_NO_BOOTLOADER := true
+
+# Display
+TARGET_SCREEN_DENSITY := 480
+
+# Kernel (vendor_boot based)
 BOARD_BOOTIMG_HEADER_VERSION := 4
 BOARD_KERNEL_IMAGE_NAME := Image
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2
-BOARD_INCLUDE_DTB_IN_BOOTIMG :=
+BOARD_INCLUDE_DTB_IN_BOOTIMG := true
 
 TARGET_FORCE_PREBUILT_KERNEL := true
 TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/kernel
 
-# vendor_boot
+# vendor_boot / recovery handling
 BOARD_MOVE_RECOVERY_RESOURCES_TO_VENDOR_BOOT := true
 BOARD_INCLUDE_RECOVERY_RAMDISK := true
 
@@ -50,22 +57,21 @@ BOARD_FLASH_BLOCK_SIZE := 262144
 BOARD_BOOTIMAGE_PARTITION_SIZE := 67108864
 BOARD_VENDOR_BOOTIMAGE_PARTITION_SIZE := 67108864
 
+BOARD_HAS_LARGE_FILESYSTEM := true
+
 BOARD_USERDATAIMAGE_FILE_SYSTEM_TYPE := f2fs
+BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
+
 BOARD_SYSTEMIMAGE_FILE_SYSTEM_TYPE := erofs
-BOARD_SYSTEM_EXTIMAGE_FILE_SYSTEM_TYPE := erofs
-BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := erofs
-BOARD_VENDOR_DLKMIMAGE_FILE_SYSTEM_TYPE := erofs
 BOARD_PRODUCTIMAGE_FILE_SYSTEM_TYPE := erofs
 BOARD_ODMIMAGE_FILE_SYSTEM_TYPE := erofs
-BOARD_ODM_DLKMIMAGE_FILE_SYSTEM_TYPE := erofs
-BOARD_SYSTEM_DLKMIMAGE_FILE_SYSTEM_TYPE := erofs
 
 TARGET_COPY_OUT_VENDOR := vendor
 
-# Dynamic partitions
-BOARD_SUPER_PARTITION_SIZE := 9126805504
+# Dynamic partitions (matches lpdump)
+BOARD_SUPER_PARTITION_SIZE := 14394851328
 BOARD_SUPER_PARTITION_GROUPS := oplus_dynamic_partitions
-BOARD_OPLUS_DYNAMIC_PARTITIONS_SIZE := 9122611200
+BOARD_OPLUS_DYNAMIC_PARTITIONS_SIZE := 14390657024
 
 BOARD_OPLUS_DYNAMIC_PARTITIONS_PARTITION_LIST := \
     system \
@@ -75,24 +81,34 @@ BOARD_OPLUS_DYNAMIC_PARTITIONS_PARTITION_LIST := \
     vendor_dlkm \
     product \
     odm \
-    odm_dlkm
+    odm_dlkm \
+    my_product \
+    my_engineering \
+    my_company \
+    my_carrier \
+    my_region \
+    my_heytap \
+    my_stock \
+    my_preload \
+    my_bigball \
+    my_manifest
 
 # Recovery
 TARGET_RECOVERY_PIXEL_FORMAT := BGRA_8888
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
 
-# AVB
-BOARD_AVB_ENABLE := true
-BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
-
-# Encryption
+# Encryption (FBE, file-based, ICE)
 TW_INCLUDE_CRYPTO := true
 TW_INCLUDE_CRYPTO_FBE := true
 TW_USE_FSCRYPT_POLICY := 2
 BOARD_USES_METADATA_PARTITION := true
 
-# Anti-rollback
+# AVB
+BOARD_AVB_ENABLE := true
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
+
+# Anti-rollback bypass (TWRP only)
 PLATFORM_SECURITY_PATCH := 2099-12-31
 VENDOR_SECURITY_PATCH := 2099-12-31
 PLATFORM_VERSION := 16.1.0
